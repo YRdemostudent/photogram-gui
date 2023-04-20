58bf6fc1fc58da72ed06576a1b11056f1bdfe7e9
Yanet Roman <yanet_0688@yahoo.com>
2023-04-20 21:20:13 +0000

    added route to username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
     
   render(:template => "user_templates/index.html.erb" )
   end 
+
+  def show
+    #Parameters: {"path_username"=>"anisa"}
+  url_username = params.fetch("path_username")
+  matching_usernames = User.where({:username => url_username})
+  
+  @the_user = matching_usernames.at(0)
+
+  # if @the_user == nil another way to do it, but you will need to put it in each class
+  #   redirect_to("/404")
+  # else 
+    render(:template => "user_templates/show.html.erb")
+    #end
+  end 
 end

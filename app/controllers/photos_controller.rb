515f4e77684829d7b9285971ea9567422d917799
Yanet Roman <yanet_0688@yahoo.com>
2023-04-21 20:45:28 +0000

    photo details not working yet

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,15 @@ def index
 
   render(:template => "photo_templates/index.html.erb")
 end
+
+def show
+  #Parameters: {"path_id"=>"777"}to guide me in my params
+
+  url_id = params.fetch("path_id")
+  
+  matching_photos = Photo.where({:id => "url_id"})
+  @the_photo = matching_photos.at(0)
+
+  render(:template => "photo_templates/show.html.erb")
+end 
 end 

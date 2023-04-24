65eca17fe54f0bc50b6d0126ccc1a1e09a77e47d
Yanet Roman <yanet_0688@yahoo.com>
2023-04-24 16:17:52 +0000

    added delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,9 +12,23 @@ def show
 
   url_id = params.fetch("path_id")
   
-  matching_photos = Photo.where({:id => "url_id"})
+  matching_photos = Photo.where({:id => url_id })
   @the_photo = matching_photos.at(0)
 
   render(:template => "photo_templates/show.html.erb")
 end 
+
+def delete 
+#Parameters: {"burn_id"=>"777"}
+the_id = params.fetch("burn_id")
+
+matching_photos = Photo.where({:id => the_id})
+the_photo = matching_photos.at(0)
+
+the_photo.destroy
+
+ #render(:template => "photo_templates/delete.html.erb")
+ redirect_to("/photos")
+end
+
 end 

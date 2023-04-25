72b85443ca935c1b0c634e7aa6ed962b214a2814
Yanet Roman <yanet_0688@yahoo.com>
2023-04-25 15:52:23 +0000

    photo creare works

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -31,4 +31,23 @@ the_photo.destroy
  redirect_to("/photos")
 end
 
+def create
+#Parameters: {"query_image"=>"an", "query_caption"=>"booo", "query_owner_id"=>"1"}Parameters: {"query_image"=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIgMRtbqu6hvSxZvbTvqcyxr-yCW5ayY_5vIkKwV299A&usqp=CAU&ec=48600112", "query_caption"=>"Life proof boats", "query_owner_id"=>"111"}
+input_image = params.fetch("query_image")
+input_caption = params.fetch("query_caption")
+input_owner_id = params.fetch("query_owner_id")
+
+a_new_photo = Photo.new
+
+a_new_photo.image = input_image
+a_new_photo.caption = input_caption
+a_new_photo.owner_id = input_owner_id
+
+a_new_photo.save
+
+#render(:template => "photo_templates/create.html.erb")
+
+redirect_to("/photos/ + a_new_photo.id.to_s")
+end 
+
 end 
